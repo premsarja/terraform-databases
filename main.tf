8a52a0fdffd2353d46c7bc158704955c28da808d
premsarja <premsagar@prodot.in>
2024-03-14 18:24:43 +0530

    done

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ module "redis" {
   source                         = "./vendor/modules/redis"
   ENV                            = var.ENV
 }
+
+module "mysql" {
+  source                         = "./vendor/modules/mysql"
+  ENV                            = var.ENV
+}
