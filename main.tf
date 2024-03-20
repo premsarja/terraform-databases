7653128fcad3661b4aa1a29f42b4e3181e3b83bb
premsarja <premsagar@prodot.in>
2024-03-20 18:45:22 +0530

    done

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "docdb" {
   ENV                            = var.ENV
   DOCDB_INSTANCE_TYPE            = var.DOCDB_INSTANCE_TYPE
   DOCDB_INSTANCE_COUNT           = var.DOCDB_INSTANCE_COUNT
-
 }
 
 module "redis" {
