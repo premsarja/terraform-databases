51df6cef2c37affdbeb5cefaba7cdcd4149fb5ea
premsarja <premsagar@prodot.in>
2024-03-20 18:57:58 +0530

    done

--- variables.tf
+++ variables.tf
@@ -2,3 +2,5 @@ variable "ENV" {}
 variable "MYSQL_STORAGE" {}
 variable "MYSQL_ENGINE_VERSION" {}
 variable "INSTANCE_CLASS" {}
+variable "DOCDB_INSTANCE_TYPE" {}
+variable "DOCDB_INSTANCE_COUNT"  {}
