28ac8c1df418cb0b6f3f8b60ca5da6e3d82332fb
premsarja <premsagar@prodot.in>
2024-03-20 17:38:36 +0530

    done

--- env-dev/dev.tfvars
+++ env-dev/dev.tfvars
@@ -1,3 +1,11 @@
 ENV = "dev"
 
+# mysql values
+MYSQL_STORAGE = 10
+MYSQL_ENGINE_VERSION = 5.7
+INSTANCE_CLASS = "db.t3.micro"
+
+# docdb value
+DOCDB_INSTANCE_TYPE = "db.t3.medium"
+DOCDB_INSTANCE_COUNT = 1
 
--- main.tf
+++ main.tf
@@ -6,9 +6,17 @@ module "docdb" {
 module "redis" {
   source                         = "./vendor/modules/redis"
   ENV                            = var.ENV
+  DOCDB_INSTANCE_TYPE            = var.DOCDB_INSTANCE_TYPE
+  DOCDB_INSTANCE_COUNT           = var.DOCDB_INSTANCE_COUNT
+
 }
 
 module "mysql" {
   source                         = "./vendor/modules/mysql"
   ENV                            = var.ENV
+  MYSQL_STORAGE                  = var.MYSQL_STORAGE
+  MYSQL_ENGINE_VERSION           = var.MYSQL_ENGINE_VERSION
+  INTANCE_CLASS                  = var.INSTANCE_CLASS
+  
+
 }
